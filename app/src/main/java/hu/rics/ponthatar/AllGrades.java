ca3b758c76b57a0f48a146bf4c69ed61b5bf3110
Rics <richard_szabo@jataka.hu>
2017-05-10 16:25:46 +0200

    New TestPaperType enum containing default values of grade percentages for different paper types. New AllGrades class containing grades 2-5.

---
+++ app/src/main/java/hu/rics/ponthatar/AllGrades.java
@@ -0,0 +1,38 @@
+package hu.rics.ponthatar;
+
+import lombok.Getter;
+
+public class AllGrades {
+
+    @Getter
+    TestPaperType testPaperType;
+    Grade grade5;
+    Grade grade4;
+    Grade grade3;
+    Grade grade2;
+
+    AllGrades() {
+        setTestPaperType(TestPaperType.SZODOLGOZAT);
+        grade5 = new Grade();
+        grade4 = new Grade();
+        grade3 = new Grade();
+        grade2 = new Grade();
+    }
+
+    void setTestPaperType(TestPaperType testPaperType) {
+        this.testPaperType = testPaperType;
+        setGrades();
+    }
+
+    void setGrades() {
+        grade2.setMinimalPercentage(testPaperType.grade2MinimalPercentage);
+        grade2.setMaximalPercentage(testPaperType.grade3MinimalPercentage-1);
+        grade3.setMinimalPercentage(testPaperType.grade3MinimalPercentage);
+        grade3.setMaximalPercentage(testPaperType.grade4MinimalPercentage-1);
+        grade4.setMinimalPercentage(testPaperType.grade4MinimalPercentage);
+        grade4.setMaximalPercentage(testPaperType.grade5MinimalPercentage-1);
+        grade5.setMinimalPercentage(testPaperType.grade5MinimalPercentage);
+        grade5.setMaximalPercentage(100);
+    }
+
+}
---
+++ app/src/main/java/hu/rics/ponthatar/TestPaperType.java
@@ -0,0 +1,19 @@
+package hu.rics.ponthatar;
+
+public enum TestPaperType {
+    TEMAZARO(40,55,70,85),
+    SZODOLGOZAT(51,64,77,89);
+    int grade2MinimalPercentage;
+    int grade3MinimalPercentage;
+    int grade4MinimalPercentage;
+    int grade5MinimalPercentage;
+    TestPaperType(int grade2MinimalPercentage,
+                  int grade3MinimalPercentage,
+                  int grade4MinimalPercentage,
+                  int grade5MinimalPercentage) {
+        this.grade2MinimalPercentage = grade2MinimalPercentage;
+        this.grade3MinimalPercentage = grade3MinimalPercentage;
+        this.grade4MinimalPercentage = grade4MinimalPercentage;
+        this.grade5MinimalPercentage = grade5MinimalPercentage;
+    }
+}
