b6406e5a2929ca51f206e2bad257d66bf73eb9c1
Rics <richard_szabo@jataka.hu>
2017-05-16 14:53:09 +0200

    GradeImplWithUIFields sets UI fields based on GradeImpl values.

--- app/src/main/java/hu/rics/ponthatar/GradeImplWithUIFields.java
+++ app/src/main/java/hu/rics/ponthatar/GradeImplWithUIFields.java
@@ -11,7 +11,26 @@ public class GradeImplWithUIFields extends GradeImpl {
     @Getter @Setter
     TextView maximalPercentageField;
     @Getter @Setter
-    TextView minimalValueField;
+    TextView minimalPointField;
     @Getter @Setter
-    TextView maximalValueField;
+    TextView maximalPointField;
+
+    @Override
+    public void setMinimalPercentage(int minimalPercentage) {
+        super.setMinimalPercentage(minimalPercentage);
+        minimalPercentageField.setText(Integer.toString(minimalPercentage));
+    }
+
+    @Override
+    public void setMaximalPercentage(int maximalPercentage) {
+        super.setMaximalPercentage(maximalPercentage);
+        maximalPercentageField.setText(Integer.toString(maximalPercentage));
+    }
+
+    @Override
+    public void calculatePointsFromOverallMaximum(int overallMaximalPoint) {
+        super.calculatePointsFromOverallMaximum(overallMaximalPoint);
+        minimalPointField.setText(Integer.toString(minimalPoint));
+        maximalPointField.setText(Integer.toString(maximalPoint));
+    }
 }
